a131ee28f76396272f1a1beec69ded1c51bbf7a7
RosTS26 <2612rostik@gmail.com>
2024-03-22 19:53:39 +0200

    1.3

--- resources/views/controlBlock.php
+++ resources/views/controlBlock.php
@@ -1,9 +1,9 @@
 <div class="control-panel d-flex col">
     <button type="button" class="btn btn-primary btn-show-modal" data-bs-toggle="modal" data-bs-target="#addUpdateModal">Add</button>
     <select class="form-select select-options" aria-label="Default select example">
-        <option value="" selected>-Please-Select-</option>
-        <option value="1">Set Active</option>
+        <option selected>-Please-Select-</option>
         <option value="0">Set Not Active</option>
+        <option value="1">Set Active</option>
         <option value="2">Delete</option>
     </select>
     <button type="button" class="btn btn-primary ok-btn">OK</button>
